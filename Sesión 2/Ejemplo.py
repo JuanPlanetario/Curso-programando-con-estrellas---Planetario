1df6106a2e32b21edc2e408c1c8038cc04ceebd3
Juan Sebastián Rodríguez Camero <juan.rodriguezc@idartes.gov.co>
2024-09-12 21:06:36 +0000

    Creación archivos de ejemplo

--- Sesión 2/Ejemplo.py
+++
@@ -1,9 +0,0 @@
-a =   #Defina la variable con el número que va a determinar si es múltiplo de 2
-b =   #Defina la variable que va a tener el residuo entre el número a y el número 2
-
-if %  =  : #Complete según la primera condición
-  print() #Use esta función para imprimir el primer mensaje
-else %  =  : #Complete según la segunda condición
-  print() #Use esta función para imprimir el segundo mensaje
-
-#Compile el código
