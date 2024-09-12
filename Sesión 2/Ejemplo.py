2f1be603f8a968bc37abcd9411f165dbf26fd696
Juan Sebastián Rodríguez Camero <juan.rodriguezc@idartes.gov.co>
2024-09-12 21:00:22 +0000

    cambios

--- Sesión 2/Ejemplo.py
+++ Sesión 2/Ejemplo.py
@@ -1,3 +1,9 @@
-def suma(a,b):
-  c = a+b
-  return c
+a =   #Defina la variable con el número que va a determinar si es múltiplo de 2
+b =   #Defina la variable que va a tener el residuo entre el número a y el número 2
+
+if %  =  : #Complete según la primera condición
+  print() #Use esta función para imprimir el primer mensaje
+else %  =  : #Complete según la segunda condición
+  print() #Use esta función para imprimir el segundo mensaje
+
+#Compile el código
