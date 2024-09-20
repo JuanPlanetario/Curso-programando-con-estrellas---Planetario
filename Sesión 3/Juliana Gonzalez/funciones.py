c35d02f7d79eccb9ade553ed0c4cdf8573b5d736
Juliana González <49600283+JulianaGonzalez@users.noreply.github.com>
2024-09-20 02:01:45 +0000

    mi libreria

---
+++ Sesión 3/Juliana Gonzalez/funciones.py
@@ -0,0 +1,25 @@
+# %%
+def suma(x,y): #Diligenciar según lo visto
+
+    return x+y
+
+# %%
+def resta(x,y): #Diligenciar según lo visto
+
+    return x-y
+
+# %%
+def mul(x,y): #Diligenciar según lo visto
+
+    return x*y
+
+# %%
+def div(x,y): #Diligenciar según lo visto
+
+    return x/y
+
+# %%
+def potencia(x,y):
+    return x**y
+
+
