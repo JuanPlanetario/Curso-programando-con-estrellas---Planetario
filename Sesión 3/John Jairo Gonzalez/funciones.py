bf1b793afdbe690cb4955eb0339adae31edc2b68
johnjg89 <soulyen@gmail.com>
2024-09-20 01:53:57 +0000

    hola

---
+++ Sesión 3/John Jairo Gonzalez/funciones.py
@@ -0,0 +1,17 @@
+# %%
+def suma(a:int,b:int): #Diligenciar según lo visto
+    return a +b
+
+# %%
+def resta(a:int,b:int): #Diligenciar según lo visto
+    return a -b
+
+# %%
+def mul(a:int,b:int): #Diligenciar según lo visto
+    return a+b
+
+# %%
+def div(a:int,b:int): #Diligenciar según lo visto
+    return  a /b
+
+
