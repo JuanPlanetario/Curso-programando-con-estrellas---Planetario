a7043722f03ed11428f07d91776cdec54e98b11d
GABY1309 <64142000+GABY1309@users.noreply.github.com>
2024-09-20 02:00:31 +0000

    hola

---
+++ Sesión 3/Gabriela Martinez/funciones.py
@@ -0,0 +1,25 @@
+# %%
+def suma(a,b): #Diligenciar según lo visto
+    resultado = a + b
+    return resultado
+
+# %%
+def resta(a,b): #Diligenciar según lo visto
+    resultado = a-b
+    return resultado 
+
+# %%
+def mul(a,b): #Diligenciar según lo visto
+    resultado = a*b
+    return resultado
+
+# %%
+def div(a,b): #Diligenciar según lo visto
+    resultado = a/b
+    return resultado 
+
+# %%
+def pow(x, y):
+    return x**y
+
+
