8a50a0f4af41751d6e61aa4875c6c0345323ce98
johnjg89 <soulyen@gmail.com>
2024-09-20 01:20:21 +0000

    Mi mensaje

---
+++ Sesión 3/John Jairo Gonzalez/funciones.py
@@ -0,0 +1,21 @@
+# %%
+def suma(): #Diligenciar según lo visto
+    return
+
+# %%
+def resta(): #Diligenciar según lo visto
+    return
+
+# %%
+def mul(): #Diligenciar según lo visto
+    return
+
+# %%
+def div(): #Diligenciar según lo visto
+    return
+
+# %%
+def pow(x, y):
+    return x**y
+
+
