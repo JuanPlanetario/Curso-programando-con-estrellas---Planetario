c2908c056ca7332b75477bad46eba0e16068958d
Alejandro Galvis <159713229+alejgalmoral@users.noreply.github.com>
2024-09-20 02:00:49 +0000

    hola

---
+++ Sesión 3/Alejandro_Galvis/funciones.py
@@ -0,0 +1,20 @@
+# %%
+def suma(a,b): #Diligenciar según lo visto
+    return a + b
+
+# %%
+def resta(c,d): #Diligenciar según lo visto
+    return c - d
+
+# %%
+def mul(e,f): #Diligenciar según lo visto
+    return e * f 
+
+# %%
+def div(g,h): #Diligenciar según lo visto
+    return g / h
+
+# %%
+div(20,3)
+
+
