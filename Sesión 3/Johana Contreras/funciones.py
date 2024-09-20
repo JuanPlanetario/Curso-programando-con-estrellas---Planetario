802e7451942d09487ddcce6ec230440bdd096759
JohanaRed666 <trileidytao@gmail.com>
2024-09-20 03:15:22 +0000

    hola

---
+++ Sesión 3/Johana Contreras/funciones.py
@@ -0,0 +1,21 @@
+# %%
+def suma(x,y): #Diligenciar según lo visto
+    return x + y
+
+# %%
+def resta(x,y): #Diligenciar según lo visto
+    return x - y
+
+# %%
+def mul(x,y): #Diligenciar según lo visto
+    return x*y
+
+# %%
+def div(x,y): #Diligenciar según lo visto
+    return x/y
+
+# %%
+def pow(x, y):
+    return x**y
+
+
